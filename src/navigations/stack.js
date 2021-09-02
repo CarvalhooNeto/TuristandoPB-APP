6944deb05443e375947ec8f6f022b282cbadf9b8
Aderaldo Neto <aderaldo.carvalho@dcx.ufpb.br>
2021-09-02 15:18:21 -0300

    adicionando pagina atrativo

--- src/navigations/stack.js
+++ src/navigations/stack.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack"
 
 
 import Inicial from '../../src/pages/inicial'
+import Atrativo from '../../src/pages/atrativo'
 import {BottomTabNavigator} from'../navigations/tab'
 
 const Stack = createStackNavigator()
@@ -14,8 +15,10 @@ const InicialStackNavigator = () => {
         <Stack.Navigator
         screenOptions={{headerShown:false}}>
             <Stack.Screen name="Inicial" component={ Inicial } />
+            <Stack.Screen name="Atrativo" component={Atrativo}/>
             <Stack.Screen name="Home" component={ BottomTabNavigator }/>
         </Stack.Navigator>
     )
 }
+
 export {InicialStackNavigator}
